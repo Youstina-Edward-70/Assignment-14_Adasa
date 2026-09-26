d989e91e784d779833bddc78571f832ccd910e03
Youstina-Edward-70 <youstinaedward1@gmail.com>
2026-09-26 19:50:49 +0300

    feat: add NotFound component with routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { Blog } from './blog/blog';
 import { About } from './about/about';
+import { NotFound } from './not-found/not-found';
 
 export const routes: Routes = [
   { path: "", redirectTo: "home", pathMatch: 'full' },
   { path: "home", component: Home },
   { path: "blog", component: Blog },
   { path: "about", component: About },
-  // {path:"", component:Home},
+  { path: "not-found", component: NotFound },
+  { path: "**", redirectTo: "not-found", pathMatch: 'full' },
 ];
